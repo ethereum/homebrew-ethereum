004340fd9b8f279eb1a14081b46726e91bd9e5ca
Vincent Gariepy <caktux@gmail.com>
2014-03-07 23:22:20 -0500

    Import ethereum.rb and README

--- README.md
+++ README.md
@@ -2,3 +2,41 @@ homebrew-ethereum
 =================
 
 Homebrew Tap for Ethereum
+
+##Installation
+
+```
+brew tap caktux/ethereum
+brew install ethereum
+brew link ethereum --overwrite
+brew linkapps
+```
+
+Then `open /Applications/AlethZero.app` or run `eth` (with `-i` for interactive mode)
+
+The `brew link ethereum --overwrite` is optional, it overwrites what `make install` put in `/usr/local`, so that if you do `brew remove ethereum` it also cleans that up.
+
+To get the latest, do:
+```
+brew install ethereum --HEAD
+```
+
+These brews can be installed via the raw GitHub URLs, or by cloning this
+repository locally with `brew tap caktux/ethereum`.
+
+##Options
+
+See `brew info ethereum`
+
+
+##Patching
+
+First `cd /Library/Caches/Homebrew/ethereum--git/` and make your changes. Then `git diff > shiny.patch`, copy/paste the content of your patch under `__END__` of `ethereum.rb` and replace the `def patches` block with:
+
+```
+def patches
+  DATA
+end
+```
+
+If you want to submit your change, save your patch in a gist, add your `option 'shiny-option', 'Shiny description'` and the URL to your gist in the patches block and submit a pull request. Make sure to send a pull request to Ethereum also!
---
+++ ethereum.rb
@@ -0,0 +1,87 @@
+require 'formula'
+
+class Ethereum < Formula
+		
+	homepage 'https://github.com/ethereum/cpp-ethereum'
+	head 'https://github.com/ethereum/cpp-ethereum.git', :branch => 'develop'
+	url 'https://github.com/ethereum/cpp-ethereum.git', :revision => '566eae4cab4cddc61b778a0033a4da958c1ad6ee'
+	# url 'https://github.com/ethereum/cpp-ethereum.git', :branch => 'develop'
+	# url 'https://github.com/ethereum/cpp-ethereum.git', :branch => 'release-poc-3'
+	version '0.3.11'
+
+	depends_on 'cmake' => :build
+	depends_on 'boost' => "--c++11"
+	# depends_on 'pkg-config' => :build
+	depends_on 'qt5'
+	depends_on 'cryptopp'
+	depends_on 'miniupnpc'
+	depends_on 'leveldb'
+	depends_on 'gmp'
+	depends_on 'ncurses' if build.include? 'with-ncurses'
+
+	option 'build-release', "Develop or Release"
+	option 'with-ncurses', "Try the ncurses patch"
+	option 'with-faucet', "Try the faucet patch"
+
+	def patches
+		# inreplace 'eth/CMakeLists.txt' do |s|
+		#   s.gsub! "replace", "with"
+		#   # s.remove_make_var! %w[CFLAGS LDFLAGS CC LD]
+		#   # s.change_make_var! "CC", ENV.cc
+		# end
+
+		urls = [
+		  ["with-ncurses", "https://gist.githubusercontent.com/caktux/9377648/raw/90988252773381f42c7ee2c1a53aa0faa98c7017/ethereum-cli-ncurses.patch"],
+		  ["with-faucet", "https://gist.githubusercontent.com/caktux/9335964/raw/77033978f5fab8c7cab87135b29d1fdf095351db/faucet-develop.patch"],
+		]
+
+		p = []
+		urls.each do |u|
+		  p << u[1] if build.include? u[0]
+		end
+
+		return p
+
+		# Uncomment below to use a patch added after __END__
+		# DATA
+	end
+
+	def install
+			# system "cmake"
+
+			args = [
+				# "--prefix=#{prefix}",
+				# "PKG_CONFIG_PATH=#{HOMEBREW_PREFIX}/opt/curl/lib/pkgconfig:#{HOMEBREW_PREFIX}/opt/ethereum/lib/pkgconfig"
+				# "--enable-some-flag"
+			]
+
+			# args << "--build-release" if build.include? '--build-release'
+
+			if build.include? "--build-release"
+				args << "-DCMAKE_BUILD_TYPE=Release"
+			elsif build.include? "--with-faucet"
+				args << "-DCMAKE_BUILD_TYPE=Faucet"
+			elsif build.include? "--with-ncurses"
+				args << "-DCMAKE_BUILD_TYPE=ncurses"
+			else
+				args << "-DCMAKE_BUILD_TYPE=Develop"
+			end
+
+			system "cmake", *args
+			system "make", "install"
+			# system "make install"
+
+			bin.install 'eth/eth'
+			prefix.install 'alethzero/AlethZero.app'
+			lib.install Dir['libethereum/*.dylib']
+			lib.install Dir['secp256k1/*.dylib']
+			# prefix.install Dir['*']
+	end
+
+	test do
+			system "eth"
+	end
+
+end
+
+__END__
