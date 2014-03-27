b9449b2922c55be68a2504cc42317c16e9057483
Vincent Gariepy <caktux@gmail.com>
2014-03-26 23:24:33 -0400

    update ncurses patch for branch develop and call

--- ethereum.rb
+++ ethereum.rb
@@ -4,9 +4,9 @@ class Ethereum < Formula
 		
 	homepage 'https://github.com/ethereum/cpp-ethereum'
 	head 'https://github.com/ethereum/cpp-ethereum.git', :branch => 'call'
-	url 'https://github.com/ethereum/cpp-ethereum.git', :revision => '153c7c36cb56a48a54de7ed86faefa6c7568fb3f'
+	url 'https://github.com/ethereum/cpp-ethereum.git', :revision => 'e0036b5f278627e3212aea83d6b85ac467ee9243'
 	# url 'https://github.com/ethereum/cpp-ethereum.git', :branch => 'release-poc-3'
-	version '0.4.0-v8-brew-18' # official_version-protocol_version-brew_version
+	version '0.4.0-v8-brew-19' # official_version-protocol_version-brew_version
 	devel do
 		url 'https://github.com/ethereum/cpp-ethereum.git', :branch => 'develop'
 	end
@@ -28,8 +28,8 @@ class Ethereum < Formula
 
 	def patches
 		inreplace 'libethereum/CMakeLists.txt' do |s|
-		  # s.gsub! "replace", "with"
 		  s.gsub! "install( TARGETS", "# install( TARGETS"
+		  # s.gsub! "replace", "with"
 		  # s.remove_make_var! %w[CFLAGS LDFLAGS CC LD]
 		  # s.change_make_var! "CC", ENV.cc
 		end
@@ -38,12 +38,12 @@ class Ethereum < Formula
 		end
 
 		urls = [
-		  ["with-ncurses", "https://gist.githubusercontent.com/caktux/9377648/raw/b89ea7f13630c81083accd79ec3755696ebfe92c/ethereum-cli-ncurses.patch"],
+		  ["with-ncurses", "https://gist.githubusercontent.com/caktux/9377648/raw/43405218a3700849ae899605d2ec27568c17288c/ethereum-cli-ncurses.patch"],
 		  ["with-export", "https://gist.githubusercontent.com/caktux/9615529/raw/907681da69a866f0e9eedd2ff68fde78e38cd43d/export-after-ncurses.patch"],
 		  ["with-faucet", "https://gist.githubusercontent.com/caktux/9335964/raw/77033978f5fab8c7cab87135b29d1fdf095351db/faucet-develop.patch"],
 		]
 
-		urls[0][1] = "https://gist.githubusercontent.com/caktux/9377648/raw/268a41c1d29badd2b3c33a8912e985888176b4a2/ethereum-cli-ncurses.patch" if build.devel?
+		urls[0][1] = "https://gist.githubusercontent.com/caktux/9377648/raw/a8d6bd800a34d48db2111ba683879888b7421f93/ethereum-cli-ncurses.patch" if build.devel?
 
 		p = []
 		p << urls[0][1] if build.include? 'with-export' and !build.include? 'with-ncurses'
@@ -58,7 +58,6 @@ class Ethereum < Formula
 	end
 
 	def install
-			# system "cmake"
 
 			args = [
 				# "--prefix=#{prefix}",
@@ -86,7 +85,6 @@ class Ethereum < Formula
 
 			system "cmake", *args
 			system "make", "install"
-			# system "make install"
 
 			bin.install 'eth/eth'
 			if !build.include? "headless"
@@ -102,5 +100,4 @@ class Ethereum < Formula
 	end
 
 end
-
 __END__
