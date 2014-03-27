596aed5420c7a38d20a4828de45e21e1401d86a0
Vincent Gariepy <caktux@gmail.com>
2014-03-27 18:52:53 -0400

    update export patch, un-g_logPost patch for now

--- ethereum.rb
+++ ethereum.rb
@@ -6,7 +6,7 @@ class Ethereum < Formula
 	head 'https://github.com/ethereum/cpp-ethereum.git', :branch => 'call'
 	url 'https://github.com/ethereum/cpp-ethereum.git', :revision => 'be1923212bea42f3f5450e98f1c6798b8ea8a6aa'
 	# url 'https://github.com/ethereum/cpp-ethereum.git', :branch => 'release-poc-3'
-	version '0.4.0-v9-brew-21' # official_version-protocol_version-brew_version
+	version '0.4.0-v9-brew-22' # official_version-protocol_version-brew_version
 	devel do
 		url 'https://github.com/ethereum/cpp-ethereum.git', :branch => 'develop'
 	end
@@ -39,7 +39,7 @@ class Ethereum < Formula
 
 		urls = [
 		  ["with-ncurses", "https://gist.githubusercontent.com/caktux/9377648/raw/a8d6bd800a34d48db2111ba683879888b7421f93/ethereum-cli-ncurses.patch"],
-		  ["with-export", "https://gist.githubusercontent.com/caktux/9615529/raw/81ecdfe0d3e187eec79430ad2f30f115afe2e68d/export-after-ncurses.patch"],
+		  ["with-export", "https://gist.githubusercontent.com/caktux/9615529/raw/68a2f44953a1773db09af1cd20d0fd6788298bae/export-after-ncurses.patch"],
 		  ["with-faucet", "https://gist.githubusercontent.com/caktux/9335964/raw/4591ad61cc43888b0be7a49eec8f987bc30c010b/faucet-develop.patch"],
 		]
 
@@ -49,6 +49,7 @@ class Ethereum < Formula
 		end
 
 		p = []
+		p[0] = "https://gist.githubusercontent.com/caktux/956c9bb6fb6728ebd810/raw/eed5c6663cfb639ef33f4ce6a75c8cb1786e0f57/un-g_logPost.patch"
 		p << urls[0][1] if build.devel? and build.include? 'with-export' and !build.include? 'with-ncurses'
 		urls.each do |u|
 		  p << u[1] if build.include? u[0]
@@ -56,7 +57,8 @@ class Ethereum < Formula
 
 		return p
 
-		# Uncomment below to use a patch added after __END__
+		# Uncomment below and comment above to use a patch added after __END__
+		# or add your patch to p[]
 		# DATA
 	end
 
