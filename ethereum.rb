8f0e80089905845c14d566f4c92b44ca9c89ec44
Vincent Gariepy <caktux@gmail.com>
2014-04-06 06:48:37 -0400

    curly standard and wrefresh seemed to segfault

--- ethereum.rb
+++ ethereum.rb
@@ -35,7 +35,7 @@ class Ethereum < Formula
 
     # Patches
     urls = [
-      ["with-forms", "https://gist.githubusercontent.com/caktux/aa6554f911f442f14faa/raw/12ab9bc96b2bd83b0443002df7f81e96eb2ad075/holy-forms.patch"],
+      ["with-forms", "https://gist.githubusercontent.com/caktux/aa6554f911f442f14faa/raw/ecb7565bf9b56ecd9c74f212315b60e98791f50c/holy-forms.patch"],
       ["with-export", "https://gist.githubusercontent.com/caktux/9615529/raw/de0c99d48dac683e5d1b8d3621db6499cd69b2ba/export-after-ncurses.patch"],
       ["with-faucet", "https://gist.githubusercontent.com/caktux/9335964/raw/216a5a7c7bd9df1525b3b48f319651804d2fb626/faucet-develop.patch"],
     ]
