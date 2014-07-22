68a0e143f65284dcb5af8ad3624bb0d323ccd84f
caktux <caktux@gmail.com>
2014-07-22 01:25:52 -0400

    jsonrpc already fixed

--- jsonrpc.rb
+++ jsonrpc.rb
@@ -19,7 +19,7 @@ class Jsonrpc < Formula
   end
 
   def patches
-    p = ["https://github.com/cinemast/libjson-rpc-cpp/pull/51.patch"]
+    p = []
 
     p << "https://gist.githubusercontent.com/caktux/b876fb0bf638f02387a8/raw/0b4ae5ab9ede6f4d88b5a5143437de00d92aa052/jsonrpc-allow-origin.patch" if build.include? "allow-origin"
 
