f63aa2c7cc69f5194e55f2c7e3e4eeb2b88169f6
caktux <caktux@gmail.com>
2014-08-21 13:46:50 -0400

    bump eth-go and go-ethereum to 0.6.3-28

--- eth-go.rb
+++ eth-go.rb
@@ -3,7 +3,7 @@ require 'formula'
 class EthGo < Formula
 
   # official_version-protocol_version
-  version '0.6.3-27'
+  version '0.6.3-28'
 
   homepage 'https://github.com/ethereum/eth-go'
   head 'https://github.com/ethereum/eth-go.git', :branch => 'develop'
