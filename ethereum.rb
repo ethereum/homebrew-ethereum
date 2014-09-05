a35776d880bc1ee09feba6531da98b556eb042d0
caktux <caktux@gmail.com>
2014-09-05 15:15:00 -0400

    manually fix version numbers while cpt-obvious updates

--- ethereum.rb
+++ ethereum.rb
@@ -11,10 +11,10 @@ class Ethereum < Formula
 
   devel do
     if build.include? "stable"
-    version '-32-1'
+    version '0.6.8-32-1'
       url 'https://github.com/ethereum/cpp-ethereum.git', :revision => '515a1c22a9e85f616ae58aa05b45e6b613e4b777'
     else
-    version '-32-1'
+    version '0.6.8-32-1'
     url 'https://github.com/ethereum/cpp-ethereum.git', :branch => 'develop'
     end
   end
