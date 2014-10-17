816fa69151ace7c6db7487726cc7ba01e5562fcc
caktux <caktux@gmail.com>
2014-10-17 12:34:37 -0400

    add mkdir_p 'build' to jsonrpc

--- jsonrpc.rb
+++ jsonrpc.rb
@@ -13,6 +13,7 @@ class Jsonrpc < Formula
   option "allow-origin", "Patch to add Access-Control-Allow-Origin: * in headers"
 
   def install
+    mkdir_p "build"
     system "cd", "build"
     system "cmake", *std_cmake_args
     system "make", "install"
