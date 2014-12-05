b34b513605dd7c204b7168f043da7cc46c9855e4
caktux <caktux@gmail.com>
2014-12-05 06:41:36 -0500

    bump go-ethereum to 0.7.8-48

--- go-ethereum.rb
+++ go-ethereum.rb
@@ -9,7 +9,7 @@ class GoEthereum < Formula
   url 'https://github.com/ethereum/go-ethereum.git', :branch => 'master'
 
   devel do
-    version '0.7.8-47'
+    version '0.7.8-48'
     url 'https://github.com/ethereum/go-ethereum.git', :branch => 'develop'
   end
 
