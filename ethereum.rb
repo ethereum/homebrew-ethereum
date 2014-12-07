574a12188ea456f08e49b95d0edff0fc39815ef9
caktux <caktux@gmail.com>
2014-12-07 07:39:18 -0500

    bump successful to 0.7.12-48-5 at b7d3d4c2fa963e8fedccac5459f481cf48e38030

--- ethereum.rb
+++ ethereum.rb
@@ -12,7 +12,7 @@ class Ethereum < Formula
   devel do
     if build.include? "successful"
       version '0.7.12-48-5'
-      url 'https://github.com/ethereum/cpp-ethereum.git', :revision => 'c0c7e5fa6d12ff13a9e90537963583f8c87cd07e'
+      url 'https://github.com/ethereum/cpp-ethereum.git', :revision => 'b7d3d4c2fa963e8fedccac5459f481cf48e38030'
     else
     version '0.7.12-48-5'
     url 'https://github.com/ethereum/cpp-ethereum.git', :branch => 'develop'
