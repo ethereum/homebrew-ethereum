5ce22f89bb5467f93890c51dddff58275ec55363
caktux <caktux@gmail.com>
2014-12-11 13:03:40 -0500

    bump to 0.7.12-49-5

--- ethereum.rb
+++ ethereum.rb
@@ -14,7 +14,7 @@ class Ethereum < Formula
       version '0.7.12-48-5'
       url 'https://github.com/ethereum/cpp-ethereum.git', :revision => '72278f87f230fc4c61c20201aa16040c449f8070'
     else
-    version '0.7.12-48-5'
+    version '0.7.12-49-5'
     url 'https://github.com/ethereum/cpp-ethereum.git', :branch => 'develop'
     end
   end
