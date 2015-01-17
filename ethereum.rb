226c987f2f709cff4e2dd5225920009f919e63d4
caktux <caktux@gmail.com>
2015-01-16 19:15:23 -0500

    bump successful to 0.8.0-51-5 at 7e1057c42a01a12b843c2c5eec2adf54d1b70cf0

--- ethereum.rb
+++ ethereum.rb
@@ -16,14 +16,14 @@ class Ethereum < Formula
 
   devel do
     bottle do
-      revision 197
-      root_url 'http://build.ethdev.com/builds/OSX%20C%2B%2B%20develop%20brew/197/bottle'
-      sha1 '1b84f214fc317866a674616ae2d6bd524e272eef' => :yosemite
+      revision 198
+      root_url 'http://build.ethdev.com/builds/OSX%20C%2B%2B%20develop%20brew/198/bottle'
+      sha1 'ad3912dd8c762839b4bea58cc37dcabe145bc9b9' => :yosemite
     end
 
     if build.include? "successful"
       version '0.8.0-51-5'
-      url 'https://github.com/ethereum/cpp-ethereum.git', :revision => 'd0f80eb441719d6ef889b94b1186d3a075bc1ee4'
+      url 'https://github.com/ethereum/cpp-ethereum.git', :revision => '7e1057c42a01a12b843c2c5eec2adf54d1b70cf0'
     else
     version '0.8.0-51-5'
     url 'https://github.com/ethereum/cpp-ethereum.git', :branch => 'develop'
