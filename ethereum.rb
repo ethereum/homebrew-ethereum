d5643abc4b670e929222e1736c790f3e364b349b
cpt-obvious <cpt-obvious@Mac-mini.local>
2015-04-04 12:27:21 -0400

    bump ethereum to 0.9.6-60 at ethereum/go-ethereum@7a18a39

--- ethereum.rb
+++ ethereum.rb
@@ -16,9 +16,9 @@ class Ethereum < Formula
 
   devel do
     bottle do
-      revision 388
-      root_url 'https://build.ethdev.com/builds/OSX%20Go%20develop%20brew/388/bottle'
-      sha1 '5fb2173a87779869a4ba616d79b2066f4c52f777' => :yosemite
+      revision 389
+      root_url 'https://build.ethdev.com/builds/OSX%20Go%20develop%20brew/389/bottle'
+      sha1 '3cc725cb9ee39501194ed5364a171e0839464b45' => :yosemite
     end
 
     version '0.9.6-60'
