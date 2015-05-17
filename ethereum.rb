fe8b26cbd6eb75ac5520d9e0a6d04cc147613c01
cpt-obvious <cpt-obvious@Mac-mini.local>
2015-05-17 16:40:32 -0400

    bump ethereum to 0.9.22-60 at ethereum/go-ethereum@915fc0e

--- ethereum.rb
+++ ethereum.rb
@@ -3,15 +3,15 @@ require 'formula'
 class Ethereum < Formula
 
   # official_version-protocol_version
-  version '-60'
+  version '0.9.22-60'
 
   homepage 'https://github.com/ethereum/go-ethereum'
   url 'https://github.com/ethereum/go-ethereum.git', :branch => 'master'
 
   bottle do
-    revision 149
-    root_url 'https://build.ethdev.com/builds/OSX%20Go%20master%20brew/149/bottle'
-    sha1 'c7837f5a926585e3d774529327122f724aed7edb' => :yosemite
+    revision 150
+    root_url 'https://build.ethdev.com/builds/OSX%20Go%20master%20brew/150/bottle'
+    sha1 '43f7a42be7acc2fda78d3a731c8b6d98561226cd' => :yosemite
   end
 
   devel do
