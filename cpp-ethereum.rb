2b63aa4e61f960c2df22153934955f46638df3ee
cpt-obvious <cpt-obvious@Mac-mini.local>
2015-07-28 12:28:52 -0400

    bump successful to 0.9.34-61 at ethereum/cpp-ethereum@fdf7635

--- cpp-ethereum.rb
+++ cpp-ethereum.rb
@@ -16,13 +16,13 @@ class CppEthereum < Formula
 
   devel do
     bottle do
-      revision 1028
-      root_url 'https://build.ethdev.com/builds/OSX%20C%2B%2B%20develop%20brew/1028/bottle'
-      sha1 'a59f0bf302950ecd7ef565a724010a85e7ecdce4' => :yosemite
+      revision 1029
+      root_url 'https://build.ethdev.com/builds/OSX%20C%2B%2B%20develop%20brew/1029/bottle'
+      sha1 '3ebbe64b3c33a32a2d8267c23aba301c725fa753' => :yosemite
     end
 
     if build.include? "successful"
-      version '-61'
+      version '0.9.34-61'
       url 'https://github.com/ethereum/cpp-ethereum.git', :revision => 'fdf7635167437f5e1a839d0284d119956450794d'
     else
     version '0.9.34-61'
