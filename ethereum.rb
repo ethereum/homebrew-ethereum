b02fb1f31aea14767f90b27d143474e6f00147e2
cpt-obvious <cpt-obvious@Mac-mini-two.local>
2015-08-17 09:05:28 -0400

    bump ethereum to 1.0.1 at ethereum/go-ethereum@ff1f6fa

--- ethereum.rb
+++ ethereum.rb
@@ -16,8 +16,8 @@ class Ethereum < Formula
 
   devel do
     bottle do
-      revision 795
-      root_url 'https://build.ethdev.com/builds/OSX%20Go%20develop%20brew/795/bottle'
+      revision 797
+      root_url 'https://build.ethdev.com/builds/OSX%20Go%20develop%20brew/797/bottle'
       sha1 'b3404722ca8dc51e7ecaa393ee3898f28caa269d' => :yosemite
     end
 
