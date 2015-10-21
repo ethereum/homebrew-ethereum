25a7e76855c5d72ee461f1899686c81a73d12716
cpt-obvious <cpt-obvious@Mac-mini.local>
2015-10-21 09:59:40 -0400

    bump ethereum to 1.2.3 on master

--- ethereum.rb
+++ ethereum.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Ethereum < Formula
 
   # official_version-protocol_version
-  version '1.2.2'
+  version '1.2.3'
 
   homepage 'https://github.com/ethereum/go-ethereum'
   url 'https://github.com/ethereum/go-ethereum.git', :branch => 'master'
