223ac59b19081379958f833c16fecfdd735bd7d8
macosx_slave <eth.jenkins.bot@gmail.com>
2015-11-12 04:15:17 +0100

    update cpp-ethereum.rb

--- cpp-ethereum.rb
+++ cpp-ethereum.rb
@@ -8,16 +8,16 @@ class CppEthereum < Formula
   url 'https://github.com/ethereum/webthree-umbrella.git', :branch => 'develop'
 
   bottle do
-    revision 117
+    revision 118
     root_url 'https://build.ethdev.com/cpp-binaries-data/brew_receipts'
-    sha1 '7e948d7015c81af4c44870faf9c2ed03b8a473d8' => :yosemite
+    sha1 '9607309ccd84841997d7ecfd195d523c9fbd96dc' => :yosemite
   end
 
   devel do
     bottle do
-      revision 117
+      revision 118
       root_url 'https://build.ethdev.com/cpp-binaries-data/brew_receipts'
-      sha1 '7e948d7015c81af4c44870faf9c2ed03b8a473d8' => :yosemite
+      sha1 '9607309ccd84841997d7ecfd195d523c9fbd96dc' => :yosemite
     end
 
     if build.include? "successful"
