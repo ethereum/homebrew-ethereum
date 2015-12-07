e3902d9de3b0299163fbde59ad3dcd5f5c429955
cpt-obvious <cpt-obvious@inval.id>
2015-12-07 05:32:50 -0500

    ethereum 1.4.0 on develop at ethereum/go-ethereum@2a3322e for El Capitan

--- ethereum.rb
+++ ethereum.rb
@@ -15,10 +15,10 @@ class Ethereum < Formula
 
   devel do
     bottle do
-      revision 43
+      revision 44
       root_url 'https://build.ethdev.com/builds/bottles-dev'
       sha256 '862e14887eaadb51cae104531f6a3e13d250bac6d919f15b17625c2f15dfa095' => :yosemite
-      sha256 '0e4df5c3b3169607bec1d1d9a244f87698bdcd798106abb85ea1ad298007da02' => :el_capitan
+      sha256 'de4368f5b6f474fc55b11fa16c503fdd7a49d5e5a1ac064ab145398d1addcec0' => :el_capitan
     end
 
     version '1.4.0'
