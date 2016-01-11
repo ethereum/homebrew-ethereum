c192d37cf0e20773073b837f3b795290175ccbd4
cpt-obvious <cpt-obvious@inval.id>
2016-01-11 10:53:54 -0500

    ethereum 1.4.0 on develop at ethereum/go-ethereum@1b8566a for Yosemite

--- ethereum.rb
+++ ethereum.rb
@@ -17,7 +17,7 @@ class Ethereum < Formula
     bottle do
       revision 56
       root_url 'https://build.ethdev.com/builds/bottles-dev'
-      sha256 '42c48b12cf797ebe6679e59dab1406c8776bf410df70f140744e5d9eb28c47af' => :yosemite
+      sha256 '26758bef4a161f87c1cb985d47a799a4ed0d84d95d0bc6b6021de32c0e8a3d35' => :yosemite
       sha256 'e30f9094afb10b2c7c5d90eb1dd8cfaee91c8de76c423717f2ded655094333c5' => :el_capitan
     end
 
