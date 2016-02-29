7f5b1c2055d2985340d338c88eea4983bd2911d5
cpt-obvious <cpt-obvious@inval.id>
2016-02-29 15:03:55 -0500

    bump ethereum to 1.3.4 on master

--- ethereum.rb
+++ ethereum.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Ethereum < Formula
-  version '1.3.3'
+  version '1.3.4'
 
   homepage 'https://github.com/ethereum/go-ethereum'
   url 'https://github.com/ethereum/go-ethereum.git', :branch => 'master'
