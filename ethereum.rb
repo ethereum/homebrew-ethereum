311f4eeb8bfc20feb9c5356e2a85f50d35035aa9
cpt-obvious <cpt-obvious@inval.id>
2016-05-13 06:25:01 -0400

    ethereum 1.5.0 on develop at ethereum/go-ethereum@fe532a9 for El Capitan

--- ethereum.rb
+++ ethereum.rb
@@ -15,10 +15,10 @@ class Ethereum < Formula
 
   devel do
     bottle do
-      revision 188
+      revision 189
       root_url 'https://build.ethdev.com/builds/bottles-dev'
       sha256 'c96f7fd137ce8629b8ab03511ceb04e174b93a3747818c09fc390709b1c7a149' => :yosemite
-      sha256 'e1492295bbc5affccc8df87becc0d6d6013bc8a25c6e91f060587e29b8a069e0' => :el_capitan
+      sha256 '9624953ce24e2b253472f338c70ea794d712f49bba8fe714937350e736761e71' => :el_capitan
     end
 
     version '1.5.0'
