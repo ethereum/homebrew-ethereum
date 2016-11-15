358f547640cd0ab05722e3034b6a503582992a42
Felix Lange <fjl@twurst.com>
2016-11-15 12:22:46 +0100

    remove geth bottles

--- ethereum.rb
+++ ethereum.rb
@@ -6,21 +6,7 @@ class Ethereum < Formula
   homepage 'https://github.com/ethereum/go-ethereum'
   url 'https://github.com/ethereum/go-ethereum.git', :branch => 'master'
 
-  bottle do
-    revision 25
-    root_url 'https://builds.ethereum.org/builds/bottles'
-    sha256 '0196f2f717e353e1a9d76add03f2af05bb840abd8bc0a097f596db86b20aa7b4' => :yosemite
-    sha256 '7056d2679661b83f6209d61c55aa30b98046120b92d6eee5d5bbb81aca15d934' => :el_capitan
-  end
-
   devel do
-    bottle do
-      revision 239
-      root_url 'https://builds.ethereum.org/builds/bottles-dev'
-      sha256 'fee2228f931dfcc3ecfef49fa64d13ae258558ecf9cffa94bad9dce65fa9fa1d' => :yosemite
-      sha256 '841ef222439404b526c7aa6585c10ea3885e5109f55359364ae135a227f917f7' => :el_capitan
-    end
-
     version '1.5.0'
     url 'https://github.com/ethereum/go-ethereum.git', :branch => 'develop'
   end
@@ -37,7 +23,7 @@ class Ethereum < Formula
   end
 
   test do
-    system "make", "test"
+    system "#{HOMEBREW_PREFIX}/bin/geth", "--version"
   end
 
   def plist; <<-EOS.undent
