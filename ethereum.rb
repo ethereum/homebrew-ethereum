d08c372c703a85993c464c30d508aa2762fab9b9
Felix Lange <fjl@twurst.com>
2016-11-30 23:56:17 +0100

    go-ethereum 1.5.4

--- ethereum.rb
+++ ethereum.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Ethereum < Formula
   homepage 'https://github.com/ethereum/go-ethereum'
-  url 'https://github.com/ethereum/go-ethereum.git', :tag => 'v1.5.3'
+  url 'https://github.com/ethereum/go-ethereum.git', :tag => 'v1.5.4'
 
   devel do
     url 'https://github.com/ethereum/go-ethereum.git', :branch => 'master'
