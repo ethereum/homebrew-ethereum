2de22833e3182e68d16ee490e8a62f556a0c5a5f
Alex Beregszaszi <alex@rtfs.hu>
2017-05-04 16:39:23 +0100

    Update to Solidity 0.4.11

--- solidity.rb
+++ solidity.rb
@@ -19,9 +19,9 @@ class Solidity < Formula
 
   desc "The Solidity Contract-Oriented Programming Language"
   homepage "http://solidity.readthedocs.org"
-  url "https://github.com/ethereum/solidity/releases/download/v0.4.10/solidity_0.4.10.tar.gz"
-  version "0.4.10"
-  sha256 "a1103f91d240101a90c9bec5243f2436563d3772c4571a352ea7bc21de8706d1"
+  url "https://github.com/ethereum/solidity/releases/download/v0.4.11/solidity_0.4.11.tar.gz"
+  version "0.4.11"
+  sha256 "55d3820253e6bc7a335705caba1f334fd6f4ae2ade9678ddef2bbfea92bbe4c6"
 
   depends_on "cmake" => :build
   depends_on "boost" => "c++11"
