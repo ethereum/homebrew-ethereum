68b7e750492716ec41647ec9d78b074c2c8e9a9b
Péter Szilágyi <peterke@gmail.com>
2017-06-01 12:58:00 +0300

    Release Geth 1.6.3

--- ethereum.rb
+++ ethereum.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Ethereum < Formula
   homepage 'https://github.com/ethereum/go-ethereum'
-  url 'https://github.com/ethereum/go-ethereum.git', :tag => 'v1.6.1'
+  url 'https://github.com/ethereum/go-ethereum.git', :tag => 'v1.6.3'
 
   devel do
     url 'https://github.com/ethereum/go-ethereum.git', :branch => 'master'
