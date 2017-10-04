387a35399a03cb4f41733997ab0fabb89376ff78
Felix Lange <fjl@users.noreply.github.com>
2017-10-04 09:12:27 +0200

    go-ethereum 1.7.1

--- ethereum.rb
+++ ethereum.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Ethereum < Formula
   homepage 'https://github.com/ethereum/go-ethereum'
-  url 'https://github.com/ethereum/go-ethereum.git', :tag => 'v1.7.0'
+  url 'https://github.com/ethereum/go-ethereum.git', :tag => 'v1.7.1'
 
   devel do
     url 'https://github.com/ethereum/go-ethereum.git', :branch => 'master'
