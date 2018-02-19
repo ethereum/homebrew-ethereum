827d8b58c598f4f393605b5ad3aaf54af7ce3bb4
Felix Lange <fjl@twurst.com>
2018-02-19 14:02:57 +0100

    Remove launchd plist for go-ethereum

--- ethereum.rb
+++ ethereum.rb
@@ -23,29 +23,4 @@ class Ethereum < Formula
   test do
     system "#{HOMEBREW_PREFIX}/bin/geth", "--version"
   end
-
-  def plist; <<-EOS.undent
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>KeepAlive</key>
-        <true/>
-        <key>ThrottleInterval</key>
-        <integer>300</integer>
-        <key>ProgramArguments</key>
-        <array>
-            <string>#{opt_bin}/geth</string>
-            <string>-datadir=#{prefix}/.ethereum</string>
-        </array>
-        <key>WorkingDirectory</key>
-        <string>#{HOMEBREW_PREFIX}</string>
-      </dict>
-    </plist>
-    EOS
-  end
 end
