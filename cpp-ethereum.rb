b6b26e127729b32e4a8885c14efd6611faa02068
Alex Beregszaszi <alex@rtfs.hu>
2018-03-21 20:35:53 +0000

    Remove launchd plist from cpp-ethereum

--- cpp-ethereum.rb
+++ cpp-ethereum.rb
@@ -65,31 +65,4 @@ class CppEthereum < Formula
     system "make", "install"
 
   end
-
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>KeepAlive</key>
-        <true/>
-        <key>ThrottleInterval</key>
-        <integer>300</integer>
-        <key>ProgramArguments</key>
-        <array>
-            <string>#{opt_bin}/eth</string>
-            <string>-d</string>
-            <string>#{prefix}/.ethereum</string>
-            <string>-b</string>
-        </array>
-        <key>WorkingDirectory</key>
-        <string>#{HOMEBREW_PREFIX}</string>
-      </dict>
-    </plist>
-    EOS
-  end
 end
