54112f3aa0666c1d9fddf4a2b4fee0d45dee3717
Richard Littauer <richard+github@burntfen.com>
2018-08-22 13:55:16 -0400

    Add ethereum brew copyright and instructions

--- ethereum.rb
+++ ethereum.rb
@@ -1,3 +1,19 @@
+#------------------------------------------------------------------------------
+# ethereum.rb
+#
+# Homebrew formula for ethereum.  Homebrew (http://brew.sh/) is
+# the de-facto standard package manager for OS X, and this Ruby script
+# contains the metadata used to map command-line user settings used
+# with the 'brew' command onto build options.
+#
+# Our documentation for the ethereum Homebrew setup is at:
+#
+# https://github.com/ethereum/go-ethereum/wiki/Installation-Instructions-for-Mac#installing-with-homebrew
+#
+# (c) 2014-2018 ethereum contributors.
+#------------------------------------------------------------------------------
+
+
 class Ethereum < Formula
   homepage 'https://github.com/ethereum/go-ethereum'
   url 'https://github.com/ethereum/go-ethereum.git', :tag => 'v1.8.14'
