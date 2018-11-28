b6efd824852e5e71cd128a55fc441a639ade5335
Péter Szilágyi <peterke@gmail.com>
2018-11-28 15:05:19 +0200

    Update go-ethereum to v1.8.19

--- ethereum.rb
+++ ethereum.rb
@@ -1,6 +1,6 @@
 class Ethereum < Formula
   homepage 'https://github.com/ethereum/go-ethereum'
-  url 'https://github.com/ethereum/go-ethereum.git', :tag => 'v1.8.18'
+  url 'https://github.com/ethereum/go-ethereum.git', :tag => 'v1.8.19'
 
   devel do
     url 'https://github.com/ethereum/go-ethereum.git', :branch => 'master'
