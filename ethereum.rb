30a3595d94fc6b0cf04a932d3bf07ae154bc4d04
Péter Szilágyi <peterke@gmail.com>
2019-12-06 12:28:31 +0200

    Update go-ethereum to v1.9.9

--- ethereum.rb
+++ ethereum.rb
@@ -1,7 +1,7 @@
 class Ethereum < Formula
   desc "Official Go implementation of the Ethereum protocol"
   homepage "https://github.com/ethereum/go-ethereum"
-  url "https://github.com/ethereum/go-ethereum.git", :tag => "v1.9.8"
+  url "https://github.com/ethereum/go-ethereum.git", :tag => "v1.9.9"
 
   devel do
     url "https://github.com/ethereum/go-ethereum.git", :branch => "master"
