944ce087d3d33d36ae3ad0fc52e75d5f8950b325
chriseth <chris@ethereum.org>
2021-11-09 14:31:30 +0100

    Solidity 0.8.10

--- solidity.rb
+++ solidity.rb
@@ -16,9 +16,9 @@
 class Solidity < Formula
   desc "The Solidity Contract-Oriented Programming Language"
   homepage "https://docs.soliditylang.org"
-  url "https://github.com/ethereum/solidity/releases/download/v0.8.9/solidity_0.8.9.tar.gz"
-  version "0.8.9"
-  sha256 "36643d0282998136b610f740808acb4dc6728144bb9ee70e5fd4124cda85ddab"
+  url "https://github.com/ethereum/solidity/releases/download/v0.8.10/solidity_0.8.10.tar.gz"
+  version "0.8.10"
+  sha256 "3f157aa2bc8bcbd8975fe5e41c476b3d777432dfe7c64e9d59247ee225a89ee3"
 
   depends_on "cmake" => :build
   depends_on "boost" => "c++11"
