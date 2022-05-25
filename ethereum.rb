0b1d0231ae12866463014e416440216dd6de34b0
Felix Lange <fjl@twurst.com>
2022-05-25 15:43:25 +0200

    go-ethereum v1.10.18

--- ethereum.rb
+++ ethereum.rb
@@ -1,7 +1,7 @@
 class Ethereum < Formula
   desc "Official Go implementation of the Ethereum protocol"
   homepage "https://github.com/ethereum/go-ethereum"
-  url "https://github.com/ethereum/go-ethereum.git", :tag => "v1.10.17"
+  url "https://github.com/ethereum/go-ethereum.git", :tag => "v1.10.18"
 
   head do
     url "https://github.com/ethereum/go-ethereum.git", :branch => "master"
