83ef570e083e53b9f3a4f10d34e99873fd1afc27
Star Chef <128301@mail.lsue.edu>
2023-05-30 11:18:15 +0300

    Download option for V1.11.6
    Added installation option for v1.11.6 to support ethash & pow.

---
+++ ethereum@1.11.6.rb
@@ -0,0 +1,32 @@
+# Due to version 1.12 stopped support for pow & ethash, defining a version for the latest version supporting ethash (1.11.6)
+
+class EthereumAT11 < Formula
+  desc "Official Go implementation of the Ethereum protocol"
+  homepage "https://github.com/ethereum/go-ethereum"
+  url "https://github.com/ethereum/go-ethereum.git", :tag => "v1.11.6"
+
+  head do
+    url "https://github.com/ethereum/go-ethereum.git", :branch => "master"
+  end
+
+  # Require El Capitan at least
+  depends_on :macos => :el_capitan
+
+  # Is there a better way to ensure that frameworks (IOKit, CoreServices, etc) are installed?
+  depends_on :xcode => :build
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOROOT"] = "#{HOMEBREW_PREFIX}/opt/go/libexec"
+    system "go", "env" # Debug env
+    system "make", "all"
+    bin.install "build/bin/evm"
+    bin.install "build/bin/geth"
+    bin.install "build/bin/rlpdump"
+  end
+
+  test do
+    system "#{HOMEBREW_PREFIX}/bin/geth", "--version"
+  end
+end
