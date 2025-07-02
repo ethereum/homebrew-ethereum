bd7c1e8001855265755c8c368b38d7f26a94c66f
Felix Lange <fjl@twurst.com>
2025-07-02 14:19:57 +0200

    go-ethereum v1.16.1

--- ethereum.rb
+++ ethereum.rb
@@ -1,7 +1,7 @@
 class Ethereum < Formula
   desc "Official Go implementation of the Ethereum protocol"
   homepage "https://github.com/ethereum/go-ethereum"
-  url "https://github.com/ethereum/go-ethereum.git", :tag => "v1.16.0"
+  url "https://github.com/ethereum/go-ethereum.git", :tag => "v1.16.1"
 
   head do
     url "https://github.com/ethereum/go-ethereum.git", :branch => "master"
